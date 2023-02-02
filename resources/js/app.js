38936c05d7f5ef3f3f79d68b992042e68967ba2e
Jarvis <13g7895123@gmail.com>
2023-02-02 21:06:03 +0800

    install tailwindcss

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import './bootstrap';
 
 import { createApp } from 'vue'
 import App from './vue/app.vue'
+import './../../src/input.css'
 
 // const { createApp } = Vue;
 
--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="text-red-500 text-3xl">
         Hello
     </div>
 </template>
